cd57e3f09b213626e36c719bead6fdfca5dbdc8f
panrunqiu <panrunqiu@wepie.com>
2024-03-06 10:37:43 +0800

    添加保存结果

--- app/src/main/java/com/peanut/nas/myapplication/AnotherHttpCallback.java
+++
@@ -1,8 +0,0 @@
-package com.peanut.nas.myapplication;
-
-public class AnotherHttpCallback implements DataCallback<Http> {
-//    @Override
-//    public void call(Http data) {
-//        //do nothing.
-//    }
-}
