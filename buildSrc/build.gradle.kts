31c406dc2614e221a6fbc5ae648408ecdce799df
panrunqiu <panrunqiu@wepie.com>
2024-03-06 11:25:20 +0800

    发布插件

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,7 +4,11 @@ repositories {
 }
 plugins {
     `kotlin-dsl`
+    id("maven-publish")
 }
+
+group = "com.peanut.r8checker"
+version = "1.0.0"
 
 dependencies {
     implementation("com.android.tools.build:gradle:8.2.0")
@@ -18,3 +22,38 @@ dependencies {
     compileOnly("org.ow2.asm:asm-commons:9.3")
     compileOnly("org.ow2.asm:asm-tree:9.3")
 }
+
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            // Creates a Maven publication called "release".
+//            release(MavenPublication) {
+//                // Applies the component for the release build variant.
+//                from components.release
+//                        artifact packAndroidJar
+//                        // You can then customize attributes of the publication as shown below.
+//                        groupId = 'com.wepie.weplay'
+//                artifactId = 'base-ui'
+//                version = snapshotVersion
+//            }
+//            // Creates a Maven publication called “debug”.
+//            debug(MavenPublication) {
+//                // Applies the component for the debug build variant.
+//                from components.debug
+//                        artifact packAndroidJar
+//                        groupId = 'com.wepie.weplay'
+//                artifactId = 'base-ui'
+//                version = snapshotVersion
+//            }
+//        }
+//        repositories {
+//            maven {
+//                url uri('https://maven.17zjh.com/repository/weflutter/')
+//                credentials {
+//                    username "weflutter"
+//                    password "weflutter"
+//                }
+//            }
+//        }
+//    }
+//}
